7d2ac5f4755e60d606fc8d0ba79c922a85210bde
林茂源 <lmy1220@126.com>
2023-03-21 10:26:08 +0800

    2023-03-21 新增创建路由菜单功能

---
+++ src/utils/menuUtils.js
@@ -0,0 +1,53 @@
+import router from "@/router";
+
+const componentList = {
+    BackLayout: () => import("@/Layout/BackLayout"),
+    BaseLayout: () => import("@/Layout/BaseLayout"),
+    HomeView: () => import("@/views/HomeView"),
+    ContentList: () => import("@/views/Content/ContentList"),
+    MediaList: () => import("@/views/Media/MediaList"),
+    UserList: () => import("@/views/User/UserList"),
+    ContentEditor: () => import("@/views/Content/ContentEditor"),
+}
+
+export const setRouterList = (list) => {
+    const routerList = getRouterList(list);
+    routerList.forEach(route => {
+        router.addRoute(route);
+    })
+}
+
+export const getRouterList = (list) => {
+    const treeList = [];
+    list.forEach(item => {
+        item.meta = {
+            title: item.title,
+            icon: item.icon,
+        }
+        item.component = componentList[item.component];
+        if (item.pid === 0)
+            treeList.push(item);
+    })
+    treeList.forEach(item => item.children = getTreeChildren(list, item.id))
+    return treeList;
+}
+
+export const getTreeChildren = (list, pid) => {
+    let treeList = []
+    list.forEach(item => {
+        if (item.pid === pid) {
+            item.children = getTreeChildren(list, item.id);
+            treeList.push(item);
+        }
+    })
+    return treeList;
+}
+
+export const getMenuByPid = (list, pid = 2) => {
+    const result = [];
+    list.forEach(item => {
+        if (item.pid === pid)
+            result.push(item);
+    });
+    return result;
+}
