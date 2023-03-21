8e5572766399e779621ae80d094af4d1d40f217c
林茂源 <lmy1220@126.com>
2023-03-21 10:31:59 +0800

    2023-03-21 完善视频播放器

--- src/components/VideoPlayer/index.vue
+++ src/components/VideoPlayer/index.vue
@@ -21,45 +21,52 @@ export default {
     },
   },
   mounted() {
-    setTimeout(() => {
-      this.instance = new player({
-        ...this.option,
-        theme: "#0054fe",
-        autoSize: true,
-        autoMini: true,
-        playbackRate: true,
-        setting: true,
-        aspectRatio: true,
-        screenshot: true,
-        hotkey: true,
-        pip: true,
-        mutex: true,
-        fullscreen: true,
-        fullscreenWeb: true,
-        subtitleOffset: true,
-        miniProgressBar: true,
-        plugins: [artplayerPluginControl()],
-        container: this.$refs.player,
-      });
-      this.instance.on('ready', () => {
-        console.log(`文件名称：${this.option.title}，`, `文件类型：视频，`, `时长：${transPlayTime(this.instance.duration)}`);
-      });
-      this.$nextTick(() => {
-        this.$emit("get-instance", this.instance);
-      });
-    }, 200);
+    this.resizeContainer()
+    this.instance = new player({
+      ...this.option,
+      theme: "#0054fe",
+      autoSize: true,
+      autoMini: true,
+      playbackRate: true,
+      setting: true,
+      aspectRatio: true,
+      screenshot: true,
+      hotkey: true,
+      pip: true,
+      mutex: true,
+      fullscreen: true,
+      fullscreenWeb: true,
+      subtitleOffset: true,
+      miniProgressBar: true,
+      plugins: [artplayerPluginControl()],
+      container: this.$refs.player,
+    });
+    this.instance.on('ready', () => {
+      console.log(`文件名称：${this.option.title}，`, `文件类型：视频，`, `时长：${transPlayTime(this.instance.duration)}`);
+    });
+    this.$nextTick(() => {
+      this.$emit("get-instance", this.instance);
+    });
+    this.instance.on("resize", (e) => {
+      // this.resizeContainer()
+      // document.querySelector(".art-video-player").style.width = "100%";
+      // document.querySelector(".art-video-player").style.height = "100%";
+    })
+
   },
-  watch: {
-    instance() {
-      this.$nextTick(() => {
-        this.$emit("get-instance", this.instance);
-      });
+  methods: {
+    resizeContainer() {
+      let width = this.$refs.player.offsetWidth;
+      let height = width * this.option.height / this.option.width;
+      console.log(width, height, this.option.width, this.option.height)
+      this.height = height + "px";
     }
   },
+  watch: {},
   beforeDestroy() {
     if (this.instance && this.instance.destroy) {
       this.instance.destroy(false);
-      this.inst = null;
+      this.instance = null;
     }
   },
 };
@@ -68,7 +75,31 @@ export default {
 <style lang="less" scoped>
 .container {
   width: 100%;
-  height: 100%;
-  aspect-ratio: 16/9;
+  height: 400px;
+
+  :deep(.art-video-player) {
+    .art-state {
+      width: 10%;
+      height: 10%;
+      bottom: 70px;
+    }
+
+    .art-bottom {
+      width: 90%;
+      min-width: unset;
+
+      .art-controls {
+        width: 100%;
+        overflow-x: auto;
+        overflow-y: hidden;
+
+        &::-webkit-scrollbar,
+        &::-webkit-scrollbar-track {
+          background-color: transparent;
+        }
+      }
+    }
+
+  }
 }
 </style>
