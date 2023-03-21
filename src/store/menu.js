c5f624f834bc6e33f7d592df5a1edd5ff06122d9
林茂源 <lmy1220@126.com>
2023-03-21 10:27:27 +0800

    2023-03-21 新增菜单仓库

---
+++ src/store/menu.js
@@ -0,0 +1,47 @@
+import {requestMenuList} from "@/api/menus";
+import {getMenuByPid, getTreeChildren, setRouterList} from "@/utils/menuUtils";
+
+export default {
+    namespaced: true,
+    state: {
+        menus: null,
+        mainMenus: null,
+        subMenus: null,
+        activeMenu: null
+    },
+    getters: {
+        menus(state) {
+            return state.menus
+        },
+        mainMenus(state) {
+            return state.mainMenus;
+        },
+        subMenus(state) {
+            return state.subMenus;
+        }
+    },
+    mutations: {
+        setMenus(state, data) {
+            state.menus = data
+        },
+        setMainMenu(state, data) {
+            state.mainMenus = data;
+        },
+        setSubMenu(state, data) {
+            state.subMenus = data;
+        }
+    },
+    actions: {
+        getMenus({commit}) {
+            requestMenuList().then(res => {
+                let menus = res.data;
+                setRouterList(menus);
+                commit("setMainMenu", getMenuByPid(menus));
+                commit("setMenus", menus);
+            })
+        },
+        setSubmenus({commit, state}, pid) {
+            commit("setSubMenu", getTreeChildren(state.menus, pid));
+        }
+    },
+}
