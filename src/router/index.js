22c40c9599a552ece621b67b709942a3817a4980
林茂源 <lmy1220@126.com>
2023-03-21 10:28:37 +0800

    2023-03-21 删除原有路由，修改为动态路由，根据后端数据动态更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,108 +1,130 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView'
-import BackLayout from "@/Layout/BackLayout";
-import ContentList from "@/views/Content/ContentList";
-import baseLayout from "@/Layout/BaseLayout";
-import MediaList from "@/views/Media/MediaList/index.vue";
-import UserList from "@/views/User/UserList/index.vue";
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        redirect: "/back",
-    },
-    {
-        path: "/back",
-        name: "back",
-        component: BackLayout,
-        redirect: "/back/home",
-        children: [
-            {
-                path: "home",
-                name: "home",
-                component: HomeView,
-                meta: {
-                    title: "首页"
-                }
-            },
-            {
-                path: "content",
-                component: baseLayout,
-                children: [
-                    {
-                        path: "",
-                        name: "contentList",
-                        component: ContentList,
-                        meta: {
-                            title: "内容管理",
-                            path: [
-                                {
-                                    link: "/back/home",
-                                    title: "首页"
-                                },
-                                {
-                                    link: "/back/content",
-                                    title: "内容管理"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                path: "media",
-                component: baseLayout,
-                children: [
-                    {
-                        path: "",
-                        name: "mediaList",
-                        component: MediaList,
-                        meta: {
-                            title: "媒体库",
-                            path: [
-                                {
-                                    link: "/back/home",
-                                    title: "首页"
-                                },
-                                {
-                                    link: "/back/media",
-                                    title: "媒体库"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                path: "user",
-                component: baseLayout,
-                children: [
-                    {
-                        path: "",
-                        name: "userList",
-                        component: UserList,
-                        meta: {
-                            title: "用户管理",
-                            path: [
-                                {
-                                    link: "/back/home",
-                                    title: "首页"
-                                },
-                                {
-                                    link: "/back/user",
-                                    title: "用户管理"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-]
+// const routes = [
+//     {
+//         path: '/',
+//         redirect: "/back/media",
+//     },
+//     {
+//         path: "/back",
+//         name: "back",
+//         component: BackLayout,
+//         redirect: "/back/home",
+//         children: [
+//             {
+//                 path: "home",
+//                 name: "home",
+//                 component: HomeView,
+//                 meta: {
+//                     title: "首页"
+//                 }
+//             },
+//             {
+//                 path: "content",
+//                 component: BaseLayout,
+//                 children: [
+//                     {
+//                         path: "",
+//                         name: "contentList",
+//                         component: ContentList,
+//                         meta: {
+//                             title: "内容管理",
+//                             path: [
+//                                 {
+//                                     link: "/back/home",
+//                                     title: "首页"
+//                                 },
+//                                 {
+//                                     link: "/back/content",
+//                                     title: "内容管理"
+//                                 }
+//                             ]
+//                         }
+//                     },
+//                     {
+//                         path: "edit",
+//                         name: "contentEditor",
+//                         component: ContentEditor,
+//                         props: route => ({
+//                             id: route.query.id
+//                         }),
+//                         meta: {
+//                             title: "内容管理",
+//                             path: [
+//                                 {
+//                                     link: "/back/home",
+//                                     title: "首页"
+//                                 },
+//                                 {
+//                                     link: "/back/content",
+//                                     title: "内容管理"
+//                                 },
+//                                 {
+//                                     link: "/back/content/edit",
+//                                     title: "编辑内容"
+//                                 }
+//                             ]
+//                         }
+//                     }
+//                 ]
+//             },
+//             {
+//                 path: "media",
+//                 component: BaseLayout,
+//                 children: [
+//                     {
+//                         path: "",
+//                         name: "mediaList",
+//                         component: MediaList,
+//                         props: route => ({path: route.query.path}),
+//                         meta: {
+//                             title: "媒体库",
+//                             path: [
+//                                 {
+//                                     link: "/back/home",
+//                                     title: "首页"
+//                                 },
+//                                 {
+//                                     link: "/back/media",
+//                                     title: "媒体库"
+//                                 }
+//                             ]
+//                         }
+//                     }
+//                 ]
+//             },
+//             {
+//                 path: "user",
+//                 component: BaseLayout,
+//                 children: [
+//                     {
+//                         path: "",
+//                         name: "userList",
+//                         component: UserList,
+//                         meta: {
+//                             title: "用户管理",
+//                             path: [
+//                                 {
+//                                     link: "/back/home",
+//                                     title: "首页"
+//                                 },
+//                                 {
+//                                     link: "/back/user",
+//                                     title: "用户管理"
+//                                 }
+//                             ]
+//                         }
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// ]
+
+const routes = [];
 
 const router = new VueRouter({
     routes,
@@ -113,7 +135,7 @@ const router = new VueRouter({
         if (to.hash) {
             return {selector: to.hash, behavior: 'smooth',}
         }
-        return { x: 0, y: 0 }
+        return {x: 0, y: 0}
     },
 })
 
