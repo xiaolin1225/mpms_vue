3a123b1502ad9699d8d1cd7452fc87f7bd79cb6a
林茂源 <lmy1220@126.com>
2023-03-22 22:43:44 +0800

    2023-03-22 在create()中新增请求菜单动作

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-<router-view/>
+  <router-view/>
 </template>
+<script>
+import {mapActions} from "vuex";
 
+export default {
+  methods: {
+    ...mapActions("menu", ["getMenus"])
+  },
+  created() {
+    this.getMenus();
+  }
+}
+</script>
 <style>
 </style>
