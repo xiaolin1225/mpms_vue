a8e211bc2985732b3468ce5f224f0eca6504174e
林茂源 <lmy1220@126.com>
2023-03-26 17:47:35 +0800

    2023-03-26 新增系统数据仓库

---
+++ src/store/system.js
@@ -0,0 +1,10 @@
+export default {
+    namespaced: true,
+    state: {
+        brand: require("@/assets/logo.png"),
+        webTitle: "融媒体平台管理系统"
+    },
+    getters: {},
+    mutations: {},
+    actions: {},
+}
